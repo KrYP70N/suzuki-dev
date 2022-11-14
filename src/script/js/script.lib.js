da55a2d28de62a33061bdf6dd208ff12bc04d12e
krypton <krypn22@gmail.com>
2022-11-14 17:08:15 +0630

    floatign menu pc view

--- src/script/js/script.lib.js
+++ src/script/js/script.lib.js
@@ -2,3 +2,63 @@
  * functions : slick slider
  */
 
+const defAniProps = {
+  easing: 'spring(1, 50, 10, 0)',
+  duration: 300
+}
+
+$('.nav--mb .ico-stack').on('click', _ => {
+  anime({
+    targets: 'header',
+    top: window.innerHeight + 'px',
+    opacity: 0,
+    ...defAniProps
+  });
+  anime({
+    targets: '#drawer-menu',
+    top: 0 + '%',
+    opacity: 1,
+    ...defAniProps
+  })
+  anime({
+    targets: '#content',
+    translateY: window.innerHeight,
+    ...defAniProps
+  })
+})
+
+$('#drawer-menu .header__close').on('click', _ => {
+  anime({
+    targets: 'header',
+    top: 0,
+    opacity: 1,
+    ...defAniProps
+  });
+  anime({
+    targets: '#drawer-menu',
+    top: -100 + '%',
+    opacity: 0,
+    ...defAniProps
+  })
+  anime({
+    targets: '#content',
+    translateY: 0,
+    ...defAniProps
+  })
+})
+
+let floatIcnState = false
+$('.float-icn').on('click', _ => {
+  floatIcnState = !floatIcnState
+  $('.float-icn').toggleClass('active')
+  anime({
+    targets: '.float-menu',
+    scale: floatIcnState ? 1 : 0,
+    right: floatIcnState ? '70px' : '30px',
+    easing: 'easeOutQuad',
+    duration: 300
+  });
+})
+
+
+
